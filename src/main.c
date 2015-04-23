9d2be18588ef969524d4da166843feea898daf03
macmania <jojofabe@vt.edu>
2015-04-23 06:13:54 -0400

    added main.c

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include "Shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv) {
+	init();
+	printPrompt();
+	firstJob = malloc(sizeof(job));
+	while(TRUE) {
+		char* cmdLine;
+		job* j = malloc(sizeof(job));
+		
+		printf("shell: ");
+		cmdLine = readCmdLine(); 
+		
+		if(isCmdEmpty(cmdLine)){
+		  continue;
+		}
+
+		//cmd = parse(cmdLine);
+		parseCommand(cmdLine, j); //takes the command and saves them to cmdType
+		
+		if(j->first_process == NULL)
+			parse(&(j->first_process), cmdLine);
+		
+		
+		if(j->first_process != NULL && isBltInCmd(j->first_process)){ 
+		  	execBltInCmd(j->first_process); //stop, etc.
+		}
+		else {
+			launchJob(j);
+		}
+	  }
+	  
+	printf("\n");
+}
